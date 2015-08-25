380818a036502c0c9a3c159aef9adf3f1836c311
reichardn <reichardn@gmail.com>
2015-08-25 23:24:41 +0000

    preliminary commit

---
+++ snippets.py
@@ -0,0 +1,37 @@
+import logging
+import argparse
+import sys
+
+# Set the log output file, and the log level
+logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
+
+
+def put(name, snippet):
+    """
+    Store a snippet with an associated name.
+
+    Returns the name and the snippet
+    """
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    return name, snippet
+
+
+def get(name):
+    """Retrieve the snippet with a given name.
+
+    If there is no such snippet return string explaining error
+
+    Returns the snippet.
+    """
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    return ""
